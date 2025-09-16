1aa553bc8a4ff6812baf93f1c68be4bb2b396675
Shashank14d <shashankdiv142@gmail.com>
2025-09-17 03:46:52 +0530

    500 error slove2

--- college_portal/settings.py
+++ college_portal/settings.py
@@ -96,7 +96,7 @@ STORAGES = {
 
 # Media files (for user-uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/college_portal/media/'
 
 STORAGES = {
     "default": {
